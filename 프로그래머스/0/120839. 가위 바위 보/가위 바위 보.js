0a9af39d9fb3989041ec2c549ae938d6f8764525
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-05-11 16:16:20 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,14 @@
+function solution(rsp) {
+    var answer = '';
+    let str = rsp.split("");
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "2") {
+            answer += "0";
+        } else if (str[i] === "0") {
+            answer += "5";
+        } else {
+            answer += "2";
+        }
+    }
+    return answer;
+}
